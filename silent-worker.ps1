4333384bc76fe272222db01644ec1a17e4e40414
Max <71935156+MaXfAa@users.noreply.github.com>
2022-04-06 01:12:06 +0200

    Update silent-worker.ps1

--- silent-worker.ps1
+++ silent-worker.ps1
@@ -1,6 +1,5 @@
-New-Item -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows" -Name Explorer
-New-ItemProperty -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows\Explorer" -Name "DisableSearchBoxSuggestions" -Type "DWord" -Value "1"
+New-Item -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows" -Name "Explorer"
 New-Item -Path "HKCU:\SOFTWARE\Microsoft\Windows" -Name "CurrentVersion"
 New-Item -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion" -Name "Search"
 Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" -Name "BingSearchEnabled" -Type "DWord" -Value "0"
-pause
+New-ItemProperty -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows\Explorer" -Name "DisableSearchBoxSuggestions" -Type "DWord" -Value "1"
